65a9ed491953e962e28bed2de65bb88f55257ba7
yaliu <yaliu@ebay.com>
2014-10-07 17:38:07 -0700

    check in

---
+++ app.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var oauthserver = require('node-oauth2-server');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var dish = require('./dish');
+
+var app = express();
+
+app.oauth = oauthserver({
+	  model: {}, // See below for specification
+	  grants: ['auth_code', 'password'],
+	  debug: true
+	});
+
+app.all('/oauth/token', app.oauth.grant());
+app.get('/', app.oauth.authorise(), function (req, res) {
+	  res.send('Secret area');
+	});
+
+app.use(app.oauth.errorHandler());
+	
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+app.get('/dish/:id', dish.getDish);
+app.get('/dish', app.oauth.authorise(), dish.getAllDishes);
+app.post('/dish', dish.addDish);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
